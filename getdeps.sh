987f71b56a21e7d73697e12dad5fdeab1d7a2ccf
Lee Azzarello <lee@seriesdigital.com>
2017-06-17 22:56:17 -0700

    get deps and ignore downloaded deps

---
+++ getdeps.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+#libs="libsigc++-2.2.10.tar.bz2 cairo-1.12.14.tar.xz gtk+-2.24.23-x11-ardour5.tar.bz2 gtk+-2.24.23-quartz-ardour5.tar.bz2 liblrdf-0.5.0.tar.gz"
+libs="gtk+-2.24.23-quartz-ardour5.tar.bz2 liblrdf-0.5.0.tar.gz"
+location="http://ardour.org/files/deps/"
+
+pushd deps
+
+for lib in $libs
+do
+  wget $location/${lib}
+done
+
+popd
